e2abedb18d368c22e2f63b2f58af59f9e8b53514
muizu <muizuvais99@gmail.com>
2024-05-07 22:17:57 +0530

    created header

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import styles from "@/components/Header.module.scss";
+
+const Header = (): JSX.Element => {
+  return (
+    <header className={styles.headerFixed}>
+      <div className={styles.headerLimiter}>
+        <h1>
+          <a href="#">
+            Healthy<span>Heart</span>
+          </a>
+        </h1>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Blog</a>
+          <a href="#">Pricing</a>
+          <a href="#">About</a>
+          <a href="#">Faq</a>
+          <a href="#">Contact</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
